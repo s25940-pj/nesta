394b2b2e63bcf1980b5e927eb90732eff4fd36fa
Maksymilian Mrówka <s25940@pjwstk.edu.pl>
2025-03-17 19:18:32 +0100

    Initial commit

---
+++ src/main/java/com/example/nesta/NestaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NestaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nesta/NestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
